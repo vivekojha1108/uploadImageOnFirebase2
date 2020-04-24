c119adf84cb405bd9ec33373fe8b3f7681c5844c
Vivek Ojha <vivekojha1108@gmail.com>
2020-04-24 23:14:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uploadimageonfirebase/ImageAdapter.java
@@ -0,0 +1,62 @@
+package com.example.uploadimageonfirebase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private Context mContext;
+    private List<Upload> mUploads;
+
+    ImageAdapter(Context context, List<Upload> uploads) {
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        Upload uploadCurrent = mUploads.get(position);
+        holder.textViewName.setText(uploadCurrent.getName());
+        Picasso.get()
+                .load(uploadCurrent.getImageUrl())
+              //  .placeholder(R.drawable.nophotoplaceholder)
+                .fit()
+                .centerCrop()
+                ////.centerInside()
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewName;
+        ImageView imageView;
+
+        ImageViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            imageView = itemView.findViewById(R.id.image_view_upload);
+        }
+    }
+}
